d454305521095cf46ce6465681fb546fc56f5661
Roman <roman.sazanoff2012@yandex.ru>
2024-03-25 12:24:49 +0300

    Задача 2 решена

--- js/arrays.js
+++ js/arrays.js
@@ -4,3 +4,17 @@ function task1(){
     let sum = arr.filter(index => index % 2 == 0).reduce((sum, elem) => sum + Math.sqrt(elem), 0);
     console.log(sum);
 }
+
+function task2(){
+    function getElem(elem){
+        if (elem >= 0 && elem <= 10){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    arr = [3, 32, 1, 59, 4, 100, 11, 10, 0, 2];
+    newArr = arr.filter(getElem).reduce((sum, elem) => sum + elem, 0);
+    console.log(newArr);
+}
